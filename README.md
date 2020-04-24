0c863cba8a0d77880e93276295f1f20f24f0cdff
NasirK24 <nasirmkhalid24@gmail.com>
2020-04-25 00:39:23 +0400

    🚀 Initialized with images and fonts

---
+++ README.md
@@ -0,0 +1,3 @@
+# GameStop
+
+A Flutter implementation of YueYue🌙's [Gamestop App Design](https://dribbble.com/shots/11134583/attachments/2737142?mode=media)
---
+++ android/app/src/main/kotlin/com/example/gamestop_dribble/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gamestop_dribble
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
